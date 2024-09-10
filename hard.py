64f05f71968cd48405a5d1a8b5256370b2af7486
Nutith <dvedenev@hotmail.com>
2024-09-10 09:11:25 +0300

    внесены изменения для соответствия примечанию задания:
    * Первое число не входит в одно из чисел пары;
    * Пары чисел подбираются от 1 до 20 для текущего числа;

--- hard.py
+++ hard.py
@@ -2,11 +2,14 @@ QUEST_NUMBERS = tuple(range(3, 21))
 CYPHER_NUMBERS = tuple(range(1, 21))
 
 
-def create_cypher_numbers_pairs(cypher_numbers):
+def create_cypher_numbers_pairs(quest_number, cypher_numbers):
     result = []
 
     for i in cypher_numbers:
         for j in cypher_numbers:
+            if j == quest_number or i == quest_number:
+                continue
+
             if i == j:
                 continue
 
@@ -28,9 +31,8 @@ def find_cypher(quest_number, cypher_numbers_pairs):
     return result
 
 
-pairs = create_cypher_numbers_pairs(CYPHER_NUMBERS)
-
 for n in QUEST_NUMBERS:
+    pairs = create_cypher_numbers_pairs(n, CYPHER_NUMBERS)
     print(f'{n} - {find_cypher(n, pairs)}')
 
 # while True:
@@ -46,6 +48,7 @@ for n in QUEST_NUMBERS:
 #         break
 #
 #     if n in QUEST_NUMBERS:
+#         pairs = create_cypher_numbers_pairs(n, CYPHER_NUMBERS)
 #         print(f'Число: {n} Шифр: {find_cypher(n, pairs)}')
 #     else:
 #         print('-- Не похоже на число со вставки, попробуйте еще раз.')
